fc883888d1863100526846edb1c0cf5bee17b2b0
kaan <kaandesu00@gmail.com>
2023-12-17 01:53:07 +0100

    chore: added rollup & vite inspect plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,34 @@
-import { defineConfig } from 'vite'
+import { defineConfig, PluginOption } from 'vite'
 import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import RadixVueResolver from 'radix-vue/resolver'
+import { visualizer } from 'rollup-plugin-visualizer'
+import Inspect from 'vite-plugin-inspect'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   if (mode === 'live-demo') {
     return {
       base: './',
-      plugins: [vue()],
+      plugins: [
+        vue(),
+        Components({
+          dts: true,
+          resolvers: [
+            RadixVueResolver(),
+
+            // RadixVueResolver({
+            //   prefix: '' // use the prefix option to add Prefix to the imported components
+            // })
+          ],
+        }),
+      ],
       test: {
         globals: true,
       },
       resolve: {
         alias: {
-          '~': resolve(__dirname, 'src', 'myComponentLibrary'),
+          '~': resolve(__dirname, 'src', 'pidekitLibrary'),
         },
       },
       build: {
@@ -23,7 +39,31 @@ export default defineConfig(({ mode }) => {
     }
   } else {
     return {
-      plugins: [vue()],
+      plugins: [
+        vue(),
+        Components({
+          dts: true,
+          resolvers: [
+            RadixVueResolver(),
+
+            // RadixVueResolver({
+            //   prefix: '' // use the prefix option to add Prefix to the imported components
+            // })
+          ],
+        }),
+        Inspect({
+          build: true,
+          outputDir: '.vite-inspect',
+        }),
+        visualizer({
+          gzipSize: true,
+          brotliSize: true,
+          open: false,
+          template: 'sunburst',
+          filename: '.rollup-inspect/index.html',
+        }), // keep this as last one
+        ,
+      ],
       test: {
         globals: true,
         setupFiles: 'src/setupTests.ts',
@@ -31,12 +71,12 @@ export default defineConfig(({ mode }) => {
       },
       resolve: {
         alias: {
-          '~': resolve(__dirname, 'src', 'myComponentLibrary'),
+          '~': resolve(__dirname, 'src', 'pidekitLibrary'),
         },
       },
       build: {
         lib: {
-          entry: resolve(__dirname, 'src/myComponentLibrary/index.ts'),
+          entry: resolve(__dirname, 'src/pidekitLibrary/index.ts'),
           name: 'Vue3ComponentLibraryTemplate',
           fileName: 'pidekit',
         },
